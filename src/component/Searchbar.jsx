4db89398ea6b5acda441d931092c48c812288643
dacdev205 <datdev252@gmail.com>
2024-05-27 15:54:35 +0700

    config css, add API ethnic

--- src/component/Searchbar.jsx
+++ src/component/Searchbar.jsx
@@ -4,9 +4,13 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { TextField } from "@mui/material";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Searchbar = () => {
   const [showSearchInfo, setShowSearchInfo] = useState(true);
+  const [ethnic, setEthnic] = useState([]);
   const [searchParams, setSearchParams] = useState({
     khoi: "",
     gioitinh: "",
@@ -21,7 +25,16 @@ const Searchbar = () => {
     console.log(e.target.value);
     setSearchParams({ ...searchParams, [name]: value });
   };
-
+  useEffect(() => {
+    function fetchAPIethnic() {
+      fetch("https://api.nosomovo.xyz/ethnic/getalllist")
+        .then((res) => res.json())
+        .then((data) => {
+          setEthnic(data);
+        });
+    }
+    fetchAPIethnic();
+  }, []);
   //   const handleSearch = () => {
   //     setFilteredStudents(
   //       students.filter((student) => {
@@ -54,50 +67,83 @@ const Searchbar = () => {
         </h1>
         {showSearchInfo && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <input
+            <TextField
+              size="small"
               type="text"
+              id="outlined-basic"
               name="hoten"
-              placeholder="Họ tên"
+              label="Họ tên"
               value={searchParams.hoten}
               onChange={handleSearchChange}
-              className="p-2 rounded bg-white text-teal-800 border-solid border-2"
+              variant="outlined"
             />
-            <input
+            <TextField
+              size="small"
               type="text"
+              id="outlined-basic"
               name="mahs"
-              placeholder="Mã học sinh"
+              label="Mã học sinh"
               value={searchParams.mahs}
               onChange={handleSearchChange}
-              className="p-2 rounded bg-white text-teal-800 border-solid border-2"
+              variant="outlined"
             />
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth size="small">
+                <InputLabel id="demo-simple-select-label">Giới tính</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={searchParams.gioitinh}
+                  label="Trạng thái"
+                  name="gioitinh"
+                  onChange={handleSearchChange}
+                >
+                  <MenuItem value="">Giới tính</MenuItem>
+                  <MenuItem value="true">Nam</MenuItem>
+                  <MenuItem value="false">Nữ</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
 
-            <select
-              name="gioitinh"
-              value={searchParams.gioitinh}
-              onChange={handleSearchChange}
-              className="p-2 rounded bg-white text-teal-800 border-solid border-2"
-            >
-              <option value="">Giới tính</option>
-              <option value="true">Nam</option>
-              <option value="false">Nữ</option>
-            </select>
-            <input
-              type="text"
-              name="dantoc"
-              placeholder="Dân tộc"
-              value={searchParams.dantoc}
-              onChange={handleSearchChange}
-              className="p-2 rounded bg-white text-teal-800 border-solid border-2"
-            />
-            <input
-              type="text"
-              name="lop"
-              placeholder="Lớp"
-              value={searchParams.lop}
-              onChange={handleSearchChange}
-              className="p-2 rounded bg-white text-teal-800 border-solid border-2"
-            />
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth size="small">
+                <InputLabel id="demo-simple-select-label">Dân tộc</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={searchParams.dantoc}
+                  label="Dân tộc"
+                  name="dantoc"
+                  onChange={handleSearchChange}
+                >
+                  {ethnic.map((item, index) => {
+                    return (
+                      <MenuItem value={item.id} key={index}>
+                        {item.name}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+            </Box>
 
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth size="small">
+                <InputLabel id="demo-simple-select-label">Lớp</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={searchParams.lop}
+                  label="lop"
+                  name="lop"
+                  onChange={handleSearchChange}
+                >
+                  <MenuItem value="">Lớp</MenuItem>
+                  <MenuItem value="Lớp 1">Lớp 1</MenuItem>
+                  <MenuItem value="Lớp 2">Lớp 2</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
             <Box sx={{ minWidth: 120 }}>
               <FormControl fullWidth size="small">
                 <InputLabel id="demo-simple-select-label">
