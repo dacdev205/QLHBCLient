3b8ab6c067801f760ad055354fa55873487234db
dacdev205 <datdev252@gmail.com>
2024-05-28 15:36:09 +0700

    add new Router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../layout/App.jsx";
 import AddStudent from "../component/AddStudent.jsx";
 import Table from "../component/Table.jsx";
+import StudentDetail from "../component/StudentDetail.jsx";
+import StudentEdit from "../component/StudentEdit.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -14,6 +16,14 @@ const router = createBrowserRouter([
       {
         path: "/addStudent",
         element: <AddStudent />,
+      },
+      {
+        path: "/student-detail/:id",
+        element: <StudentDetail />,
+      },
+      {
+        path: "/student-edit/:id",
+        element: <StudentEdit />,
       },
     ],
   },
