545559c50a6774ae7a5777c493be71c81a8d3a7c
dacdev205 <datdev252@gmail.com>
2024-05-30 16:16:12 +0700

    fix bug API address

--- src/component/AddStudent.jsx
+++ src/component/AddStudent.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -7,12 +7,13 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { TextField } from "@mui/material";
 
-import { useEffect } from "react";
 const AddStudent = () => {
   const [ethnic, setEthnic] = useState([]);
   const [city, setCity] = useState([]);
   const [district, setDistrict] = useState([]);
+  const [commune, setCommune] = useState([]);
   const [isCitySelected, setIsCitySelected] = useState(false);
+  const [isDistrictSelected, setIsDistrictSelected] = useState(false);
   const initialFormData = {
     hoTen: "",
     ngaySinh: "",
@@ -33,27 +34,23 @@ const AddStudent = () => {
     trangThai: "",
     nhapHoc: "",
   };
+
   useEffect(() => {
-    function fetchAPIethnic() {
-      fetch("https://api.nosomovo.xyz/ethnic/getalllist")
-        .then((res) => res.json())
-        .then((data) => {
-          setEthnic(data);
-        });
-    }
-    fetchAPIethnic();
+    fetch("https://api.nosomovo.xyz/ethnic/getalllist")
+      .then((res) => res.json())
+      .then((data) => {
+        setEthnic(data);
+      });
   }, []);
 
   useEffect(() => {
-    function fetchAPICity() {
-      fetch("https://api.nosomovo.xyz/province/getalllist/193")
-        .then((res) => res.json())
-        .then((data) => {
-          setCity(data);
-        });
-    }
-    fetchAPICity();
+    fetch("https://api.nosomovo.xyz/province/getalllist/193")
+      .then((res) => res.json())
+      .then((data) => {
+        setCity(data);
+      });
   }, []);
+
   const [formData, setFormData] = useState(initialFormData);
 
   const handleChange = async (e) => {
@@ -62,28 +59,48 @@ const AddStudent = () => {
       ...formData,
       [name]: type === "file" ? files[0] : value,
     });
+
     if (name === "tinh") {
-      const cityId = value;
+      const cityName = value;
       try {
         const res = await fetch(
-          `https://api.nosomovo.xyz/district/getalllist/${cityId}`
+          `https://api.nosomovo.xyz/district/getalllist/${
+            city.find((item) => item.name === cityName).id
+          }`
         );
         const data = await res.json();
         setDistrict(data);
+        setCommune([]); // Clear the commune when city is changed
         setIsCitySelected(true);
+        setIsDistrictSelected(false);
       } catch (error) {
         console.error("Error fetching districts", error);
+      }
+    }
+
+    if (name === "huyen") {
+      const districtName = value;
+      try {
+        const res = await fetch(
+          `https://api.nosomovo.xyz/commune/getalllist/${
+            district.find((item) => item.name === districtName).id
+          }`
+        );
+        const data = await res.json();
+        setCommune(data);
+        setIsDistrictSelected(true);
+      } catch (error) {
+        console.error("Error fetching communes", error);
       }
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setFormData(initialFormData);
     axios
       .post("http://192.168.234.154:8080/api/hocsinh/add", formData)
-      .then((response) => {
-        console.log(response.data);
+      .then(() => {
+        setFormData(initialFormData);
       });
   };
 
@@ -91,30 +108,9 @@ const AddStudent = () => {
     <div className="bg-white w-full min-h-screen text-black">
       <h1>Thông tin chung</h1>
       <div className="container mx-auto py-6 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 min-h-screen bg-white gap-4">
-        {/* <div>
-          <img
-            className="w-64 h-64 object-cover"
-            src={
-              formData.hinhAnh
-                ? URL.createObjectURL(formData.hinhAnh)
-                : "/istockphoto-1300845620-612x612.jpg"
-            }
-            alt=""
-          />
-          <label htmlFor="hinhAnh">Chọn ảnh:</label>
-          <input
-            type="file"
-            id="hinhAnh"
-            name="hinhAnh"
-            onChange={(e) => handleChange(e)}
-            className="border border-gray-300 rounded px-3 py-2 bg-white"
-          />
-        </div> */}
-
         <div className="col-span-1 md:col-span-1 lg:col-span-2">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mr-10">
             <div>
- 
               <div className="mb-2">
                 <TextField
                   className="w-full"
@@ -129,7 +125,7 @@ const AddStudent = () => {
                 />
               </div>
               <div className="mb-2">
-              <TextField
+                <TextField
                   className="w-full"
                   size="small"
                   type="text"
@@ -140,77 +136,81 @@ const AddStudent = () => {
                   label="Lớp"
                   variant="outlined"
                 />
-              </div> 
-                
-              
-              <div className="mb-2">
-              <Box sx={{ minWidth: 120 }} className="mb-2">
-                <FormControl fullWidth size="small">
-                  <InputLabel id="demo-simple-select-label" className="w-full">
-                    Giới tính
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Giới tính"
-                    value={formData.gioiTinh}
-                    onChange={handleChange}
-                    name="gioiTinh"
-                  >
-                    <MenuItem value="null">Giới tính</MenuItem>
-                    <MenuItem value="true">Nam</MenuItem>
-                    <MenuItem value="false">Nữ</MenuItem>
-                  </Select>
-                </FormControl>
-              </Box>
-              <div className="mb-2">
-                <TextField
-                  className="w-full"
-                  size="small"
-                  type="text"
-                  id="outlined-basic"
-                  name="maHs"
-                  value={formData.maHs}
-                  onChange={handleChange}
-                  label="Mã học sinh"
-                  variant="outlined"
-                />
               </div>
 
               <div className="mb-2">
-              <TextField
-                  className="w-full"
-                  size="small"
-                  type="text"
-                  id="outlined-basic"
-                  name="maDinhDanh"
-                  label="Mã Định Danh"
-                  value={formData.maDinhDanh}
-                  onChange={handleChange}
-                  variant="outlined"
-                />
-              </div>
-              
-              <Box sx={{ minWidth: 120 }} className="mb-2">
-                <FormControl fullWidth size="small">
-                  <InputLabel id="demo-simple-select-label" className="w-full">
-                    Trạng thái
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Giới tính"
-                    value={formData.trangThai}
+                <Box sx={{ minWidth: 120 }} className="mb-2">
+                  <FormControl fullWidth size="small">
+                    <InputLabel
+                      id="demo-simple-select-label"
+                      className="w-full"
+                    >
+                      Giới tính
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Giới tính"
+                      value={formData.gioiTinh}
+                      onChange={handleChange}
+                      name="gioiTinh"
+                    >
+                      <MenuItem value="null">Giới tính</MenuItem>
+                      <MenuItem value="true">Nam</MenuItem>
+                      <MenuItem value="false">Nữ</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+                <div className="mb-2">
+                  <TextField
+                    className="w-full"
+                    size="small"
+                    type="text"
+                    id="outlined-basic"
+                    name="maHs"
+                    value={formData.maHs}
                     onChange={handleChange}
-                    name="gioiTinh"
-                  >
-                    <MenuItem value="null">Trạng hái</MenuItem>
-                    <MenuItem value="true">Đang Học</MenuItem>
-                    <MenuItem value="false">Không Học</MenuItem>
-                  </Select>
-                </FormControl>
-              </Box>
-                
+                    label="Mã học sinh"
+                    variant="outlined"
+                  />
+                </div>
+
+                <div className="mb-2">
+                  <TextField
+                    className="w-full"
+                    size="small"
+                    type="text"
+                    id="outlined-basic"
+                    name="maDinhDanh"
+                    label="Mã Định Danh"
+                    value={formData.maDinhDanh}
+                    onChange={handleChange}
+                    variant="outlined"
+                  />
+                </div>
+
+                <Box sx={{ minWidth: 120 }} className="mb-2">
+                  <FormControl fullWidth size="small">
+                    <InputLabel
+                      id="demo-simple-select-label"
+                      className="w-full"
+                    >
+                      Trạng thái
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Trạng thái"
+                      value={formData.trangThai}
+                      onChange={handleChange}
+                      name="trangThai"
+                    >
+                      <MenuItem value="null">Trạng thái</MenuItem>
+                      <MenuItem value="true">Đang Học</MenuItem>
+                      <MenuItem value="false">Không Học</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
               </div>
               <div className="mb-2">
                 <TextField
@@ -225,7 +225,6 @@ const AddStudent = () => {
                   variant="outlined"
                 />
               </div>
-             
 
               <div className="mb-2">
                 <label htmlFor="ngaySinh">Ngày sinh:</label>
@@ -238,7 +237,7 @@ const AddStudent = () => {
                   className="border border-gray-300 rounded px-3 py-2 w-full bg-white"
                 />
               </div>
-              
+
               <div>
                 <label htmlFor="nhapHoc">Ngày nhập học:</label>
                 <input
@@ -285,13 +284,11 @@ const AddStudent = () => {
                     value={formData.tinh}
                     onChange={handleChange}
                   >
-                    {city.map((item, index) => {
-                      return (
-                        <MenuItem value={item.id} key={index}>
-                          {item.name}
-                        </MenuItem>
-                      );
-                    })}
+                    {city.map((item, index) => (
+                      <MenuItem value={item.name} key={index}>
+                        {item.name}
+                      </MenuItem>
+                    ))}
                   </Select>
                 </FormControl>
               </Box>
@@ -308,7 +305,7 @@ const AddStudent = () => {
                     disabled={!isCitySelected}
                   >
                     {district.map((item, index) => (
-                      <MenuItem key={index} value={item.id}>
+                      <MenuItem key={index} value={item.name}>
                         {item.name}
                       </MenuItem>
                     ))}
@@ -328,10 +325,13 @@ const AddStudent = () => {
                     name="xa"
                     value={formData.xa}
                     onChange={handleChange}
+                    disabled={!isDistrictSelected}
                   >
-                    <MenuItem value="">Tôn giáo</MenuItem>
-                    <MenuItem value="true">Nam</MenuItem>
-                    <MenuItem value="false">Nữ</MenuItem>
+                    {commune.map((item, index) => (
+                      <MenuItem key={index} value={item.name}>
+                        {item.name}
+                      </MenuItem>
+                    ))}
                   </Select>
                 </FormControl>
               </Box>
@@ -389,13 +389,11 @@ const AddStudent = () => {
                     name="danToc"
                     onChange={handleChange}
                   >
-                    {ethnic.map((item, index) => {
-                      return (
-                        <MenuItem value={item.id} key={index}>
-                          {item.name}
-                        </MenuItem>
-                      );
-                    })}
+                    {ethnic.map((item, index) => (
+                      <MenuItem value={item.name} key={index}>
+                        {item.name}
+                      </MenuItem>
+                    ))}
                   </Select>
                 </FormControl>
               </Box>
