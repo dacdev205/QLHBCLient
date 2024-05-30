3563ccc417a881a83f42d3828a4e9a4540cf199b
dacdev205 <datdev252@gmail.com>
2024-05-30 15:38:31 +0700

    set avt default

--- src/component/StudentDetail.jsx
+++ src/component/StudentDetail.jsx
@@ -26,12 +26,20 @@ const StudentDetail = () => {
       <h1>Thông tin chung</h1>
       <div className="container mx-auto py-6 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 min-h-screen bg-white gap-4">
         <div>
-          <img className="w-64 h-64 object-cover" src={PF} alt="" />
+          {student.hinhAnh ? (
+            <img src={PF} className="w-64 h-64 rounded-full" alt="student" />
+          ) : (
+            <img
+              src="https://cdn-icons-png.freepik.com/512/266/266033.png"
+              className="lg:w-64 lg:h-64 rounded-full md:w-32 md:h-32"
+              alt="student"
+            />
+          )}
         </div>
         <div className="col-span-1 md:col-span-1 lg:col-span-2">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mr-10">
             <div>
-            <div className="mb-4 ">Họ và tên: {student.hoTen}</div>
+              <div className="mb-4 ">Họ và tên: {student.hoTen}</div>
               <div className="mb-4">Lớp: {student.lop}</div>
               <div className="mb-4">
                 Giới tính: {student.gioiTinh ? "Nam" : "Nữ"}
@@ -40,30 +48,29 @@ const StudentDetail = () => {
               <div className="mb-4">Mã định danh: {student.maDinhDanh}</div>
               <div className="mb-4 ">
                 Trạng thái: {student.trangThai ? "Đang học" : "Không Học"}
-              </div> 
+              </div>
               <div className="mb-4">
                 Ngày sinh:{" "}
                 {student.ngaySinh
                   ? new Date(student.ngaySinh).toLocaleDateString("en-GB", {
                       day: "2-digit",
                       month: "2-digit",
-                      year: "numeric"
+                      year: "numeric",
                     })
                   : "N/A"}
               </div>
-              
+
               <div className="mb-4">
                 Ngày vào trường:{" "}
                 {student.nhapHoc
                   ? new Date(student.nhapHoc).toLocaleDateString("en-GB", {
                       day: "2-digit",
                       month: "2-digit",
-                      year: "numeric"
+                      year: "numeric",
                     })
                   : "N/A"}
               </div>
               <div className="mb-4">Nơi sinh: {student.noiSinh}</div>
-              
             </div>
 
             <div>
